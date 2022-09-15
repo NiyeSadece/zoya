6c1bcd11950635ab9207c97953da03af57e997b1
Dalia Grodzka <39556362+NiyeSadece@users.noreply.github.com>
2022-09-15 14:55:59 +0200

    defined models

--- zoya/zoya_site/admin.py
+++ zoya/zoya_site/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from zoya_site.models import DiscordUser, EXP, Message
 
-# Register your models here.
+
+admin.site.register(DiscordUser)
+admin.site.register(EXP)
+admin.site.register(Message)
--- zoya/zoya_site/models.py
+++ zoya/zoya_site/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class DiscordUser(models.Model):
+    name = models.CharField(max_length=125)
+    discord_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class EXP (models.Model):
+    user = models.OneToOneField(DiscordUser, on_delete=models.CASCADE)
+    exp = models.IntegerField(default=0)
+    lvl = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.user.name} EXP"
+
+
+class Message (models.Model):
+    name = models.CharField(max_length=125)
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.name} - wiadomość"
